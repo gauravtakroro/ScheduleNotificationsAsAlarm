5ea9bc8e0b4e4e9fb28bcf4650f6da5255e1841a
gauravtakroro <gaurav.tak@roro.io>
2025-01-30 00:10:05 +0530

    initial (Main) activity added with other changes

--- app/src/main/java/com/gauravtak/schedulenotificationsasalarm/MainActivity.kt
+++ app/src/main/java/com/gauravtak/schedulenotificationsasalarm/MainActivity.kt
@@ -3,14 +3,22 @@ package com.gauravtak.schedulenotificationsasalarm
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.gauravtak.schedulenotificationsasalarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Inside your activity's onCreate or a method
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
